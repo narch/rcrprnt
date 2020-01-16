8d41151edd9e0a4d7b5aa68da0640e280f77e1f2
Nathan Williams <nathan@nathan.la>
2020-01-16 09:52:07 -0800

    Fix migration

--- db/migrate/20100227180338_create_products_stores.rb
+++ db/migrate/20100227180338_create_products_stores.rb
@@ -4,11 +4,11 @@ class CreateProductsStores < SpreeExtension::Migration[4.2]
       t.references :product
       t.references :store
     end
-    add_index :products_stores, :product_id
-    add_index :products_stores, :store_id
+    add_index :spree_products_stores, :product_id
+    add_index :spree_products_stores, :store_id
   end
 
   def self.down
-    drop_table :products_stores
+    drop_table :spree_products_stores
   end
 end
